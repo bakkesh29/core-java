ed382416eb426596d3a7cee2eab3e3f8bbe3b50a
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-03-13 10:37:20 +0530

    Commited Day-1 folder

---
+++ Day-1/EducationInfo.java
@@ -0,0 +1,10 @@
+public class EducationInfo {
+    public static void main(String[] args) {
+        System.out.println("\n--- Education ---");
+        System.out.println("\n My Education Information ");
+        System.out.println("Name: Bakkesh Y M R");
+        System.out.println("10th School: Vishwa Chetana school,Davangere");
+        System.out.println("PU Collage: Vysnavi Chetana PU College,Davangere");
+        System.out.println("Degree College: BIET, Davangere");
+    }
+}
