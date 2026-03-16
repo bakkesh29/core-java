d1091cce966803099a555d7b75b09a4d18936854
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-03-16 20:46:42 +0530

    Commited Day-11 Task

---
+++ ReturnTypes/Primitive.java
@@ -0,0 +1,11 @@
+class Primitive{
+
+    static int getNumber() {
+        int num = 10;
+        return num;
+    }
+    public static void main(String[] args) {
+        int result = getNumber();
+        System.out.println(result);
+    }
+}
