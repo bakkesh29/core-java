fe4fe58a5f010de84d7e664d5bd4f1df63e672f2
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-03-25 22:54:50 +0530

    Add Two integers

---
+++ Aptitude-questions/AddNumbers.java
@@ -0,0 +1,15 @@
+// Declares a class named AddNumbers
+public class AddNumbers {
+    // main method
+    public static void main(String[] args) {
+
+        // Declares two integer variables a and b
+        int a = 10;
+        int b = 14;
+        // Declares another integer variable sum
+        int sum = a + b;
+        // message to the console
+        System.out.println("The sum of two integers is: " + (a + b));
+        System.out.println("The sum of two integers is: " + (sum));
+    }
+}
