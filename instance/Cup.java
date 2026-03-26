9008613dc510a5be959f226f10d5bdf85585f661
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-03-26 19:31:10 +0530

    Creating instances

---
+++ instance/Cup.java
@@ -0,0 +1,14 @@
+class Cup {
+
+    // Instance variables
+    String material;
+    String color;
+    double price;
+    String size;
+    boolean isReusable;
+
+    // No-arg constructor
+    Cup() {
+        System.out.println("Cup created");
+    }
+}
