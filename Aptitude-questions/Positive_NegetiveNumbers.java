6272b93842009fbfce9f7774a569ce86b7e008c1
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-03-28 21:46:49 +0530

    Positive_NegetiveNumbers

---
+++ Aptitude-questions/Positive_NegetiveNumbers.java
@@ -0,0 +1,16 @@
+public class Positive_NegetiveNumbers {
+    public static void main(String[] args) {
+        
+
+        double number =12.33;
+        if (number < 0.0) {
+            System.out.println(number + "is a negitive number ");
+        }
+        else if (number > 0.0) {
+            System.out.println(number + "is a positive number ");
+        }
+        else {
+            System.out.println("It is zero");
+        }
+    }
+}
--- Aptitude-questions/SwapNUmbers.java
+++ Aptitude-questions/SwapNUmbers.java
@@ -5,7 +5,10 @@ public class SwapNUmbers {
         int a = 10;
         int b = 20;
 
-        // Print values before swapping
+        int c = 30;
+        int d = 40;
+
+        // Print values before swapping (using temp variable)
         System.out.println("before swapping");
         System.out.println("a: " + a);
         System.out.println("b: " + b);
@@ -16,13 +19,32 @@ public class SwapNUmbers {
         // Temporary variable to help with swapping
         int temp;
 
-        // Swap logic:
+        // Swap logic using temp:
         temp = a;   // store value of 'a' in temp
         a = b;      // assign value of 'b' to 'a'
         b = temp;   // assign value of temp (original 'a') to 'b'
 
         // Print values after swapping
-        System.out.println("a: " + a);
-        System.out.println("b: " + b);
+        System.out.println("a: " + a); // now a = 20
+        System.out.println("b: " + b); // now b = 10
+
+
+
+        // Swap without using temp variable (using arithmetic)
+        // Print values before swapping
+        System.out.println("before swapping");
+        System.out.println("c: " + c);
+        System.out.println("d: " + d);
+
+        System.out.println("after swapping");
+
+        // Correct arithmetic swap:
+        c = c + d;  // c = 30 + 40 = 70
+        d = c - d;  // d = 70 - 40 = 30 (original c)
+        c = c - d;  // c = 70 - 30 = 40 (original d)
+
+        // Print values after swapping
+        System.out.println("c: " + c); // now c = 40
+        System.out.println("d: " + d); // now d = 30
     }
 }
