68f66d9568794c08b2f6a614be2699fbed8c081e
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-03-28 19:45:12 +0530

    Vowels or a consonent

--- Aptitude-questions/VowelAndConsonent.java
+++ Aptitude-questions/VowelAndConsonent.java
@@ -1,15 +1,29 @@
 public class VowelAndConsonent {
     public static void main(String[] args) {
-        //a e i o u 
-        char ch ='e';
+        // a e i o u
+        char ch = 'e';
 
-        if (ch=='a' || ch =='e' || ch =='i' || ch =='o' || ch == 'u'){
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
             System.out.println(ch + "  : is vowel");
-            
-        }
-        else{
+
+        } else {
             System.out.println(ch + "Consonent");
         }
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + "  :it is a vowel");
+
+                break;
+
+            default:
+                System.out.println(ch + "  :it is a consonent");
+                break;
+        }
     }
-    
+
 }
