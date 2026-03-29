e1ad68d603a08199d863a59a582027e86a356e0d
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-03-29 11:46:14 +0530

    Implemented Mouse program

---
+++ OOP-Instance-Programs/Mouse.java
@@ -0,0 +1,25 @@
+class Mouse {
+
+    // 5 literals
+    String type = "Wireless";
+    String color = "Black";
+    int dpi = 1600;
+    boolean isGaming = true;
+    double price = 999.99;
+
+    // 10 variables
+    String brand;
+    String connectivity;
+    int buttons;
+    String sensorType;
+    boolean hasRGB;
+    int warranty;
+    String weight;
+    String compatibility;
+    String shape;
+    String finish;
+
+    Mouse() {
+        System.out.println("Mouse created");
+    }
+}
