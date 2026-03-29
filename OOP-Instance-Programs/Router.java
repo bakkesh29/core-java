1819ae578399c5411a77bb907c6753f7222b8cf6
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-03-29 12:05:45 +0530

    Added Router class

---
+++ OOP-Instance-Programs/Router.java
@@ -0,0 +1,25 @@
+class Router {
+
+    // 5 literals
+    String type = "Wireless";
+    int speed = 300;
+    boolean isDualBand = true;
+    double price = 2500;
+    String brand = "TP-Link";
+
+    // 10 reference variables
+    String model;
+    int ports;
+    String frequency;
+    int warranty;
+    String color;
+    boolean hasAntenna;
+    String coverage;
+    String usage;
+    String securityType;
+    String firmware;
+
+    Router() {
+        System.out.println("Router created");
+    }
+}
