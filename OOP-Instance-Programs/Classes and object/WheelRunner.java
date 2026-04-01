a4048c1b361330cb0e1a598ce32b20372548b0db
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-04-01 22:26:19 +0530

    added Class Runner

---
+++ OOP-Instance-Programs/Classes and object/WheelRunner.java
@@ -0,0 +1,60 @@
+class WheelRunner {
+    public static void main(String... values) {
+
+        Wheel wheel1 = new Wheel("Alloy",17,"Aluminum","MRF",5000,"Car","Black",8.0,true,5);
+        Wheel wheel2 = new Wheel("Steel",16,"Steel","Apollo",4000,"Car","Silver",9.0,false,4);
+        Wheel wheel3 = new Wheel("Alloy",18,"Aluminum","CEAT",5500,"SUV","Black",8.5,true,5);
+        Wheel wheel4 = new Wheel("Steel",15,"Steel","JK",3500,"Bike","Grey",7.0,false,3);
+        Wheel wheel5 = new Wheel("Alloy",19,"Aluminum","Bridgestone",6000,"Car","Black",9.0,true,5);
+
+        Wheel wheel6 = new Wheel("Steel",17,"Steel","MRF",4200,"Car","Silver",9.5,false,4);
+        Wheel wheel7 = new Wheel("Alloy",18,"Aluminum","Apollo",5600,"SUV","Black",8.7,true,5);
+        Wheel wheel8 = new Wheel("Steel",16,"Steel","CEAT",3800,"Car","Grey",9.2,false,4);
+        Wheel wheel9 = new Wheel("Alloy",20,"Aluminum","JK",6500,"SUV","Black",9.3,true,5);
+        Wheel wheel10 = new Wheel("Steel",15,"Steel","Bridgestone",3600,"Bike","Silver",7.2,false,3);
+
+        Wheel wheel11 = new Wheel("Alloy",17,"Aluminum","MRF",5100,"Car","Black",8.1,true,5);
+        Wheel wheel12 = new Wheel("Steel",16,"Steel","Apollo",4050,"Car","Silver",9.1,false,4);
+        Wheel wheel13 = new Wheel("Alloy",18,"Aluminum","CEAT",5700,"SUV","Black",8.6,true,5);
+        Wheel wheel14 = new Wheel("Steel",15,"Steel","JK",3550,"Bike","Grey",7.1,false,3);
+        Wheel wheel15 = new Wheel("Alloy",19,"Aluminum","Bridgestone",6100,"Car","Black",9.2,true,5);
+
+        Wheel wheel16 = new Wheel("Steel",17,"Steel","MRF",4300,"Car","Silver",9.4,false,4);
+        Wheel wheel17 = new Wheel("Alloy",18,"Aluminum","Apollo",5800,"SUV","Black",8.8,true,5);
+        Wheel wheel18 = new Wheel("Steel",16,"Steel","CEAT",3900,"Car","Grey",9.3,false,4);
+        Wheel wheel19 = new Wheel("Alloy",20,"Aluminum","JK",6700,"SUV","Black",9.4,true,5);
+        Wheel wheel20 = new Wheel("Steel",15,"Steel","Bridgestone",3700,"Bike","Silver",7.3,false,3);
+
+        Wheel wheel21 = new Wheel("Alloy",17,"Aluminum","MRF",5200,"Car","Black",8.2,true,5);
+        Wheel wheel22 = new Wheel("Steel",16,"Steel","Apollo",4100,"Car","Silver",9.2,false,4);
+        Wheel wheel23 = new Wheel("Alloy",18,"Aluminum","CEAT",5900,"SUV","Black",8.9,true,5);
+        Wheel wheel24 = new Wheel("Steel",15,"Steel","JK",3600,"Bike","Grey",7.2,false,3);
+        Wheel wheel25 = new Wheel("Alloy",19,"Aluminum","Bridgestone",6200,"Car","Black",9.5,true,5);
+
+        wheel1.display();
+        wheel2.display();
+        wheel3.display();
+        wheel4.display();
+        wheel5.display();
+        wheel6.display();
+        wheel7.display();
+        wheel8.display();
+        wheel9.display();
+        wheel10.display();
+        wheel11.display();
+        wheel12.display();
+        wheel13.display();
+        wheel14.display();
+        wheel15.display();
+        wheel16.display();
+        wheel17.display();
+        wheel18.display();
+        wheel19.display();
+        wheel20.display();
+        wheel21.display();
+        wheel22.display();
+        wheel23.display();
+        wheel24.display();
+        wheel25.display();
+    }
+}
