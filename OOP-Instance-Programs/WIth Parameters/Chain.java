f8d574b6beecd0afcb4131d52d61288308118b17
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-04-05 16:59:44 +0530

    class Added

--- OOP-Instance-Programs/WIth Parameters/Chain.java
+++ OOP-Instance-Programs/WIth Parameters/Chain.java
@@ -4,26 +4,23 @@ class Chain {
     String material;
     double price;
     double length;
+    int quantity;
 
-    // Constructor 1
     Chain() {
         System.out.println("Default Constructor");
     }
 
-    // Constructor 2
     Chain(String brand, String material) {
         this.brand = brand;
         this.material = material;
     }
 
-    // Constructor 3
     Chain(String brand, String material, double price) {
         this.brand = brand;
         this.material = material;
         this.price = price;
     }
 
-    // Constructor 4
     Chain(String brand, String material, double price, double length) {
         this.brand = brand;
         this.material = material;
@@ -31,12 +28,20 @@ class Chain {
         this.length = length;
     }
 
+    Chain(String brand, String material, double price, double length, int quantity) {
+        this.brand = brand;
+        this.material = material;
+        this.price = price;
+        this.length = length;
+        this.quantity = quantity;
+    }
 
     void show() {
         System.out.println("Brand: " + brand);
         System.out.println("Material: " + material);
         System.out.println("Price: " + price);
         System.out.println("Length: " + length);
+        System.out.println("Quantity: " + quantity);
         System.out.println("------------------");
     }
 }
--- OOP-Instance-Programs/WIth Parameters/ChainRunner.java
+++ OOP-Instance-Programs/WIth Parameters/ChainRunner.java
@@ -4,13 +4,16 @@ class ChainRunner {
         Chain c1 = new Chain();
         c1.show();
 
-        Chain c2 = new Chain("Malabar", "Gold");
+        Chain c2 = new Chain("Tanishq", "Gold");
         c2.show();
 
-        Chain c3 = new Chain("Tanishq", "Silver", 8000);
+        Chain c3 = new Chain("Malabar", "Silver", 15000);
         c3.show();
 
-        Chain c4 = new Chain("Kalyan", "Platinum", 15000, 20);
+        Chain c4 = new Chain("Kalyan", "Platinum", 25000, 18);
         c4.show();
+
+        Chain c5 = new Chain("PC Jeweller", "Gold", 30000, 20, 2);
+        c5.show();
     }
 }
