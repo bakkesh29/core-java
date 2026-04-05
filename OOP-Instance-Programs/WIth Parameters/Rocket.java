fd4591788e5a98ead95f75970fad75747f6568c8
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-04-05 17:05:49 +0530

    classes Added

--- OOP-Instance-Programs/WIth Parameters/Rocket.java
+++ OOP-Instance-Programs/WIth Parameters/Rocket.java
@@ -1,49 +1,47 @@
 class Rocket {
 
     String name;
-    String fuelType;
-    double price;
     String country;
+    double cost;
+    String type;
+    int stages;
 
-    // Constructor 1
     Rocket() {
         System.out.println("Default Constructor");
     }
 
-    // Constructor 2
-    Rocket(String name, String fuelType) {
+    Rocket(String name, String country) {
         this.name = name;
-        this.fuelType = fuelType;
-    }
-
-    // Constructor 3
-    Rocket(String name, String fuelType, double price) {
-        this.name = name;
-        this.fuelType = fuelType;
-        this.price = price;
-    }
-
-    // Constructor 4
-    Rocket(String name, String fuelType, double price, String country) {
-        this.name = name;
-        this.fuelType = fuelType;
-        this.price = price;
         this.country = country;
     }
 
-    // Constructor 5 (Copy)
-    Rocket(Rocket ref) {
-        this.name = ref.name;
-        this.fuelType = ref.fuelType;
-        this.price = ref.price;
-        this.country = ref.country;
+    Rocket(String name, String country, double cost) {
+        this.name = name;
+        this.country = country;
+        this.cost = cost;
+    }
+
+    Rocket(String name, String country, double cost, String type) {
+        this.name = name;
+        this.country = country;
+        this.cost = cost;
+        this.type = type;
+    }
+
+    Rocket(String name, String country, double cost, String type, int stages) {
+        this.name = name;
+        this.country = country;
+        this.cost = cost;
+        this.type = type;
+        this.stages = stages;
     }
 
     void show() {
         System.out.println("Name: " + name);
-        System.out.println("Fuel Type: " + fuelType);
-        System.out.println("Price: " + price);
         System.out.println("Country: " + country);
+        System.out.println("Cost: " + cost);
+        System.out.println("Type: " + type);
+        System.out.println("Stages: " + stages);
         System.out.println("------------------");
     }
 }
--- OOP-Instance-Programs/WIth Parameters/RocketRunner.java
+++ OOP-Instance-Programs/WIth Parameters/RocketRunner.java
@@ -4,16 +4,16 @@ class RocketRunner {
         Rocket r1 = new Rocket();
         r1.show();
 
-        Rocket r2 = new Rocket("Falcon", "Liquid");
+        Rocket r2 = new Rocket("Falcon 9", "USA");
         r2.show();
 
-        Rocket r3 = new Rocket("GSLV", "Solid", 5000000);
+        Rocket r3 = new Rocket("PSLV", "India", 150);
         r3.show();
 
-        Rocket r4 = new Rocket("PSLV", "Hybrid", 8000000, "India");
+        Rocket r4 = new Rocket("GSLV", "India", 250, "Launch Vehicle");
         r4.show();
 
-        Rocket r5 = new Rocket(r4);
+        Rocket r5 = new Rocket("Starship", "USA", 500, "Reusable", 2);
         r5.show();
     }
 }
