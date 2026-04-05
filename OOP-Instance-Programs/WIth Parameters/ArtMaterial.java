22076e331854c9061e6c8313ab53852bb3f5214b
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-04-05 16:55:21 +0530

    class Added

--- OOP-Instance-Programs/WIth Parameters/ArtMaterial.java
+++ OOP-Instance-Programs/WIth Parameters/ArtMaterial.java
@@ -4,6 +4,7 @@ class ArtMaterial {
     String type;
     double price;
     String color;
+    int quantity;   
 
     ArtMaterial() {
         System.out.println("Default Constructor");
@@ -27,11 +28,13 @@ class ArtMaterial {
         this.color = color;
     }
 
-    ArtMaterial(ArtMaterial ref) {
-        this.brand = ref.brand;
-        this.type = ref.type;
-        this.price = ref.price;
-        this.color = ref.color;
+    
+    ArtMaterial(String brand, String type, double price, String color, int quantity) {
+        this.brand = brand;
+        this.type = type;
+        this.price = price;
+        this.color = color;
+        this.quantity = quantity;
     }
 
     void show() {
@@ -39,6 +42,7 @@ class ArtMaterial {
         System.out.println("Type: " + type);
         System.out.println("Price: " + price);
         System.out.println("Color: " + color);
+        System.out.println("Quantity: " + quantity);
         System.out.println("------------------");
     }
 }
