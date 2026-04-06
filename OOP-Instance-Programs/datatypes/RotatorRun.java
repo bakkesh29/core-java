1c7af7788b432810de07bf040ad9cb955e84c4ee
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-04-06 20:35:38 +0530

    class Added

---
+++ OOP-Instance-Programs/datatypes/RotatorRun.java
@@ -0,0 +1,9 @@
+class RotaterRun {
+    void run(Rotater rotater) {
+        if (rotater != null) {
+            rotater.show();
+        } else {
+            System.out.println("Rotater is null");
+        }
+    }
+}
