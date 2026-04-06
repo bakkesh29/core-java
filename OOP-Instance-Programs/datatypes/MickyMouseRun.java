9bcc65730fc245b4e1d6a0eddae3fb7eb8edadb3
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-04-06 20:45:34 +0530

    class Added

---
+++ OOP-Instance-Programs/datatypes/MickyMouseRun.java
@@ -0,0 +1,9 @@
+class MickeyMouseRun {
+    void run(MickeyMouse mickeyMouse) {
+        if (mickeyMouse != null) {
+            mickeyMouse.show();
+        } else {
+            System.out.println("MickeyMouse is null");
+        }
+    }
+}
