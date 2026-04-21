cb85ba69f845e91705fce446f5ac7f5da7decf34
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-04-21 17:03:24 +0530

    5 Classes with 5 Instance Variables

---
+++ Inheritance/Inheritance_And_Chaining_Assignment/Candle.java
@@ -0,0 +1,8 @@
+//5 Instance Var
+class Candle {
+    String color;
+    String scent;
+    double price;
+    int size;
+    String brand;
+}
---
+++ Inheritance/Inheritance_And_Chaining_Assignment/Hospital.java
@@ -0,0 +1,8 @@
+//5 Instance Var
+class Hospital {
+    String name;
+    String city;
+    int beds;
+    int doctors;
+    String type;
+}
