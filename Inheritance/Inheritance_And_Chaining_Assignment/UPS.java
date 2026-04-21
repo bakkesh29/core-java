b6f1523774a76d825f5f464b8fc00bd769812d73
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-04-21 17:07:09 +0530

    Super Chaining (super())

---
+++ Inheritance/Inheritance_And_Chaining_Assignment/UPS.java
@@ -0,0 +1,21 @@
+class UPS {
+    void start() {
+        System.out.println("UPS start");
+    }
+
+    void stop() {
+        System.out.println("UPS stop");
+    }
+
+    void charge() {
+        System.out.println("UPS charging");
+    }
+
+    void backup() {
+        System.out.println("UPS backup");
+    }
+
+    void shutdown() {
+        System.out.println("UPS shutdown");
+    }
+}
