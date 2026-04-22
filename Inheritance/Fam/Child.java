1406f80e59ce978469a49f503c9777187007e05a
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-04-22 23:46:08 +0530

    added sub class of Parent

---
+++ Inheritance/Fam/Child.java
@@ -0,0 +1,11 @@
+
+public class Child extends Parent{
+    void nothing(){
+        System.out.println("son has lost everything running in SOn class");
+    }
+    @Override
+    void start(){
+        System.out.println("Running start from Chlid class");
+    }
+}
+
