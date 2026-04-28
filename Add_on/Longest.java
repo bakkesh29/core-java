327810031f888a0e11723c56c4a83c15146dae52
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-04-28 21:35:40 +0530

    added longest letters

---
+++ Add_on/Longest.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Longest {
+    public static void main(String[] args) {
+
+        String arr[] = {
+            "X-workz",
+            "Omkar-sir",
+            "Bakkesh",
+            "MyCodeSchool"
+        };
+
+        // Find maximum length
+        int max = 0;
+
+        for (String s : arr) {
+            if (s.length() > max) {
+                max = s.length();
+            }
+        }
+
+        // Print longest strings
+        System.out.println("Longest String(s):");
+
+        for (String s : arr) {
+            if (s.length() == max) {
+                System.out.println(s);
+            }
+        }
+    }
+}
