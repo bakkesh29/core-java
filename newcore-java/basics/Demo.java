2e6208e9402e501c203c49eaffd07317ea688233
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-05-29 17:12:43 +0530

    added intger flow

---
+++ newcore-java/basics/Demo.java
@@ -0,0 +1,21 @@
+public class Demo {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+        // byte result = (byte)(a+b)
+        int result = a + b;
+        System.out.println(result);
+
+        int n1 = 12;
+        int n2 = 6;
+        // float res = n2/n1;
+        double res = n1 / n2;
+
+        // int res = n1/n2;
+        System.out.println(res);
+        // rounding to zero
+        // trucation
+        // when ever int by int divisionhappen resultis always integrer nothing matters
+        // what ur storing it
+    }
+}
