9b12178d1ee971ae01ceb010069bd9fdda6925ef
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-06-19 20:48:28 +0530

    added Math example.java

---
+++ Add_on/Basics/practice /MathExample.java
@@ -0,0 +1,11 @@
+public class MathExample {
+    public static void main(String[] args) {
+        int firstNumber = 10;
+        double secondNumber = 5.5;
+        
+        // Add them together
+        double sum = firstNumber + secondNumber;
+        
+        System.out.println("The sum is: " + sum);
+    }
+}
