5e4efcda1d50ff537f28c774339a04bdd7188d95
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-06-23 21:00:59 +0530

    added array elements

---
+++ newcore-java/Revise/intro to OOP's/GFG.java
@@ -0,0 +1,22 @@
+// Java Program to print the elements
+// using Arrays.toString() Method
+import java.util.Arrays;
+
+public class GFG {
+
+    public static void main(String[] args)
+    {
+
+        // integer array
+        int[] a = { 1, 2, 3, 4, 5 };
+
+        // string array
+        String[] s = { "vivek", "deeksha", "ankur" };
+
+        // Printing integer array using toString() Method
+        System.out.println(Arrays.toString(a));
+
+        // Printing string array  using toString() Method
+        System.out.println(Arrays.toString(s));
+    }
+}
