7749c79f7360807dd143d13feab09dcdc0f11ecc
Bakkesh Y M R <bakkeshymr@gmail.com>
2026-07-18 14:45:05 +0530

    added the comback plan

---
+++ Java /Java Basics  /Hello.java
@@ -0,0 +1,6 @@
+class Hello{
+    public static void main (String [] args ){
+        System.out.println("hello bakkesh this is comeback ! ")
+        System.out.println("fasten your seat belts ")
+    }
+}
